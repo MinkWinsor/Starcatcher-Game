8063b6aa84069ae99fd2bf9c3fc58bb79101f441
MinkWinsor <steven.olsen18@gmail.com>
2016-11-05 18:06:30 -0600

    Nav Mesh attempt
    Some new ideas.

---
+++ Assets/MoveEnemy.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveEnemy : MonoBehaviour {
+
+
+
+    public Transform goal;
+
+    void Start()
+    {
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.destination = goal.position;
+    }
+}
