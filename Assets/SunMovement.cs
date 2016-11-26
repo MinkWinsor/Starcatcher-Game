5a60837d140edae98bebb3466a476a3b921e32d1
MinkWinsor <steven.olsen18@gmail.com>
2016-11-26 12:24:28 -0700

    Stopped sun from moving when game freezes.

--- Assets/SunMovement.cs
+++ Assets/SunMovement.cs
@@ -5,10 +5,11 @@ public class SunMovement : MonoBehaviour {
 
     public static bool goDown = true;
     public int speed = 10;
-
-	// Use this for initialization
+    
+	
+    // Use this for initialization
 	void Start () {
-
+        KillControl.StopAllScripts += stopScript;
 	}
 	
 	void Update ()
@@ -19,5 +20,10 @@ public class SunMovement : MonoBehaviour {
             transform.position = new Vector3(transform.position.x, transform.position.y + (speed * Time.deltaTime), transform.position.z);
            
         
+    }
+
+    void stopScript()
+    {
+        this.enabled = false;
     }
 }
